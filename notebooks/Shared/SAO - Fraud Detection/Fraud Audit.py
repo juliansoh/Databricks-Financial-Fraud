a7283b1cf5d3775f10ebe2069a97ffac64367654
Julian <31254706+juliansoh@users.noreply.github.com>
2019-10-17 09:52:43 -0700

    Updated the two ML models to demonstrate better True negatives at the expense of more false positives.

--- notebooks/Shared/SAO - Fraud Detection/Fraud Audit.py
+++ notebooks/Shared/SAO - Fraud Detection/Fraud Audit.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 # MAGIC %md #<a href="http://www.sao.wa.gov"><img width="635" height="132" src="https://www.sao.wa.gov/wp-content/uploads/2018/09/WSAO_LogoLargerIcon-1.png"></a>
 # MAGIC #Financial Fraud Audit
+# MAGIC ***(Exercise. Not an actual Fraud Audit)***<br><br>
 # MAGIC <a href="https://www.linkedin.com/in/julian-soh-307ab24/">Julian Soh</a><br>
 # MAGIC October 2019<br>Microsoft Corporation
 
@@ -11,7 +12,7 @@
 # COMMAND ----------
 
 # MAGIC %md This is a demonstration of how Databricks can be used as a collaboration and analysis tool for detecting fraud in large datasets.
-# MAGIC The dataset used in this demo is the <a href="https://www.kaggle.com/ntnu-testimon/paysim1/download">Kaggle Synthetic Financial Datasets for Fraud Detection</a>
+# MAGIC The dataset used in this demo is the [Kaggle Synthetic Financial Datasets for Fraud Detection](https://www.kaggle.com/ntnu-testimon/paysim1/download)
 # MAGIC 
 # MAGIC This notebook helps the discussion of the following steps:
 # MAGIC <ul>
@@ -100,6 +101,31 @@ df.count()
 
 # COMMAND ----------
 
+# MAGIC %md ## Taxonomy
+# MAGIC step - maps a unit of time in the real world. In this case 1 step is 1 hour of time. Total steps 744 (30 days simulation).
+# MAGIC 
+# MAGIC type - CASH-IN, CASH-OUT, DEBIT, PAYMENT and TRANSFER.
+# MAGIC 
+# MAGIC amount - amount of the transaction in local currency.
+# MAGIC 
+# MAGIC nameOrig - customer who started the transaction
+# MAGIC 
+# MAGIC oldbalanceOrg - initial balance before the transaction
+# MAGIC 
+# MAGIC newbalanceOrig - new balance after the transaction
+# MAGIC 
+# MAGIC nameDest - customer who is the recipient of the transaction
+# MAGIC 
+# MAGIC oldbalanceDest - initial balance recipient before the transaction. Note that there is not information for customers that start with M (Merchants).
+# MAGIC 
+# MAGIC newbalanceDest - new balance recipient after the transaction. Note that there is not information for customers that start with M (Merchants).
+# MAGIC 
+# MAGIC isFraud - This is the transactions made by the fraudulent agents inside the simulation. In this specific dataset the fraudulent behavior of the agents aims to profit by taking control or customers accounts and try to empty the funds by transferring to another account and then cashing out of the system.
+# MAGIC 
+# MAGIC isFlaggedFraud - The business model aims to control massive transfers from one account to another and flags illegal attempts. An illegal attempt in this dataset is an attempt to transfer more than 200.000 in a single transaction.
+
+# COMMAND ----------
+
 # MAGIC %md #Start of Audit
 
 # COMMAND ----------
@@ -161,7 +187,7 @@ display(df)
 
 # COMMAND ----------
 
-# MAGIC %md For this, we will use PySpark. This is a combination of Python and Spark to address big data. <a href="https://www.edureka.co/blog/pyspark-programming/">Find out more about PySpark</a>.<br>
+# MAGIC %md For this, we will use PySpark. This is a combination of Python and Spark to address big data. [Find out more about PySpark](https://www.edureka.co/blog/pyspark-programming/)<br>
 # MAGIC  <img src="https://d1jnx9ba8s6j9r.cloudfront.net/blog/wp-content/uploads/2018/07/PySpark.png" width=548 height=74>
 
 # COMMAND ----------
@@ -254,6 +280,13 @@ print("Total rows: %s, Training rows: %s, Test rows: %s" % (df.count(), train.co
 
 # COMMAND ----------
 
+# MAGIC %md ###An ML Pipeline
+# MAGIC To create an ML model, this entails repeating steps (e.g. StringIndexer, VenctorAssmbler, etc). An ML pipeline allows you to reuse these steps with new/additional data for retraining purposes. The key is thaat the more data you have, the more accurate your ML model will be.
+
+# COMMAND ----------
+
+#Create the ML Pipeline
+
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import StringIndexer
 from pyspark.ml.feature import OneHotEncoderEstimator
@@ -280,17 +313,372 @@ display(dt_model.stages[-1])
 
 # COMMAND ----------
 
-# MAGIC %md ##Internal use only - File Operations
+# MAGIC %md ###Use BinaryClassificationEvaluator
+# MAGIC Determine the accuracy of the model by reviewing the "areaUnderPR" and "areaUnderROC"
 
 # COMMAND ----------
 
-display(dbutils.fs.ls("dbfs:/FileStore/tables/csv/sao"))
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+# Use BinaryClassificationEvaluator to evaluate our model
+evaluatorPR = BinaryClassificationEvaluator(labelCol = "label", rawPredictionCol = "prediction", metricName = "areaUnderPR")
+evaluatorAUC = BinaryClassificationEvaluator(labelCol = "label", rawPredictionCol = "prediction", metricName = "areaUnderROC")
 
 # COMMAND ----------
 
-dbutils.fs.mkdirs("/FileStore/tables/csv/sao")
+# MAGIC %md ###Setup CrossValidation
+# MAGIC To try out different parameters to potentially improve our model, we will use CrossValidator in conjunction with the ParamGridBuilder to automate trying out different parameters.
+# MAGIC Note, we are using evaluatorPR as our evaluator as the Precision-Recall curve is often better for an unbalanced distribution.
 
 # COMMAND ----------
 
-# MAGIC %fs wget https://www.sao.wa.gov/wp-content/uploads/2018/09/WSAO_LogoLargerIcon-1.png
-# MAGIC dbutils.fs.cp 
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+
+# Build the grid of different parameters
+paramGrid = ParamGridBuilder() \
+    .addGrid(dt.maxDepth, [5, 10, 15]) \
+    .addGrid(dt.maxBins, [10, 20, 30]) \
+    .build()
+
+# Build out the cross validation
+crossval = CrossValidator(estimator = dt,
+                          estimatorParamMaps = paramGrid,
+                          evaluator = evaluatorPR,
+                          numFolds = 3)  
+
+pipelineCV = Pipeline(stages=[indexer, va, crossval])
+
+# Train the model using the pipeline, parameter grid, and preceding BinaryClassificationEvaluator
+cvModel_u = pipelineCV.fit(train)
+
+# COMMAND ----------
+
+# MAGIC %md ###Review Results
+# MAGIC Review the areaUnderPR (area under Precision Recall curve) and areaUnderROC (area under Receiver operating characteristic) or AUC (area under curve) metrics
+
+# COMMAND ----------
+
+# Build the best model (training and test datasets)
+train_pred = cvModel_u.transform(train)
+test_pred = cvModel_u.transform(test)
+
+# Evaluate the model on training datasets
+pr_train = evaluatorPR.evaluate(train_pred)
+auc_train = evaluatorAUC.evaluate(train_pred)
+
+# Evaluate the model on test datasets
+pr_test = evaluatorPR.evaluate(test_pred)
+auc_test = evaluatorAUC.evaluate(test_pred)
+
+# Print out the PR and AUC values
+print("PR train:", pr_train)
+print("AUC train:", auc_train)
+print("PR test:", pr_test)
+print("AUC test:", auc_test)
+
+# COMMAND ----------
+
+# MAGIC %md ###Confusion Matrix Code-base
+# MAGIC Subsequent cells will be using the following code to plot the confusion matrix.
+
+# COMMAND ----------
+
+# Create confusion matrix template
+from pyspark.sql.functions import lit, expr, col, column
+
+# Confusion matrix template
+cmt = spark.createDataFrame([(1, 0), (0, 0), (1, 1), (0, 1)], ["label", "prediction"])
+cmt.createOrReplaceTempView("cmt")
+
+# COMMAND ----------
+
+# Source code for plotting confusion matrix is based on `plot_confusion_matrix` 
+# via https://runawayhorse001.github.io/LearningApacheSpark/classification.html#decision-tree-classification
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+def plot_confusion_matrix(cm, title):
+  # Clear Plot
+  plt.gcf().clear()
+
+  # Configure figure
+  fig = plt.figure(1)
+  
+  # Configure plot
+  classes = ['Fraud', 'No Fraud']
+  plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+  plt.title(title)
+  plt.colorbar()
+  tick_marks = np.arange(len(classes))
+  plt.xticks(tick_marks, classes, rotation=45)
+  plt.yticks(tick_marks, classes)
+
+  # Normalize and establish threshold
+  normalize=False
+  fmt = 'd'
+  thresh = cm.max() / 2.
+
+  # Iterate through the confusion matrix cells
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+      plt.text(j, i, format(cm[i, j], fmt),
+               horizontalalignment="center",
+               color="white" if cm[i, j] > thresh else "black")
+
+  # Final plot configurations
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label') 
+  
+  # Display images
+  image = fig
+  
+  # Show plot
+  #fig = plt.show()
+  
+  # Save plot
+  fig.savefig("confusion-matrix.png")
+
+  # Display Plot
+  display(image)
+  
+  # Close Plot
+  plt.close(fig)
+
+# COMMAND ----------
+
+# Create temporary view for test predictions
+test_pred.createOrReplaceTempView("test_pred")
+
+# Create test predictions confusion matrix
+test_pred_cmdf = spark.sql("select a.label, a.prediction, coalesce(b.count, 0) as count from cmt a left outer join (select label, prediction, count(1) as count from test_pred group by label, prediction) b on b.label = a.label and b.prediction = a.prediction order by a.label desc, a.prediction desc")
+
+# View confusion matrix
+display(test_pred_cmdf)
+
+# COMMAND ----------
+
+# MAGIC %md ### View Confusion Matrix
+# MAGIC Using matplotlib and pandas to visualize our confusion matrix
+
+# COMMAND ----------
+
+# Convert to pandas
+cm_pdf = test_pred_cmdf.toPandas()
+
+# Create 1d numpy array of confusion matrix values
+cm_1d = cm_pdf.iloc[:, 2]
+
+# Create 2d numpy array of confusion matrix values
+cm = np.reshape(cm_1d, (-1, 2))
+
+# Print out the 2d array
+print(cm)
+
+
+# COMMAND ----------
+
+# Plot confusion matrix  
+plot_confusion_matrix(cm, "Confusion Matrix (Unbalanced Test)")
+
+# COMMAND ----------
+
+# Log MLflow
+with mlflow.start_run(experiment_id = mlflow_experiment_id) as run:
+  # Log Parameters and metrics
+  mlflow.log_param("balanced", "no")
+  mlflow.log_metric("PR train", pr_train)
+  mlflow.log_metric("AUC train", auc_train)
+  mlflow.log_metric("PR test", pr_test)
+  mlflow.log_metric("AUC test", auc_test)
+  
+  # Log model
+  mlflow.spark.log_model(dt_model, "model")
+  
+  # Log Confusion matrix
+  mlflow.log_artifact("confusion-matrix.png")
+
+# COMMAND ----------
+
+# MAGIC %md ### Model with Balanced classes
+# MAGIC Let's see if we can improve our decision tree model but balancing the Fraud vs. No Fraud cases. We will tune the model using the metrics areaUnderROC or (AUC)
+
+# COMMAND ----------
+
+# Reset the DataFrames for no fraud (`dfn`) and fraud (`dfy`)
+dfn = train.filter(train.label == 0)
+dfy = train.filter(train.label == 1)
+
+# Calculate summary metrics
+N = train.count()
+y = dfy.count()
+p = y/N
+
+# Create a more balanced training dataset
+train_b = dfn.sample(False, p, seed = 92285).union(dfy)
+
+# Print out metrics
+print("Total count: %s, Fraud cases count: %s, Proportion of fraud cases: %s" % (N, y, p))
+print("Balanced training dataset count: %s" % train_b.count())
+
+
+# COMMAND ----------
+
+# Display our more balanced training dataset
+display(train_b.groupBy("label").count())
+
+# COMMAND ----------
+
+# MAGIC %md ### Update the ML Pipeline
+# MAGIC Because we had created the ML pipeline stages in the previous cells, we can re-use them to execute it against our balanced dataset.
+# MAGIC Note, we are using evaluatorAUC as our evaluator as this is often better for a balanced distribution.
+
+# COMMAND ----------
+
+# Re-run the same ML pipeline (including parameters grid)
+crossval_b = CrossValidator(estimator = dt,
+                          estimatorParamMaps = paramGrid,
+                          evaluator = evaluatorAUC,
+                          numFolds = 3)  
+
+pipelineCV_b = Pipeline(stages=[indexer, va, crossval_b])
+
+# Train the model using the pipeline, parameter grid, and BinaryClassificationEvaluator using the `train_b` dataset
+cvModel_b = pipelineCV_b.fit(train_b)
+
+# COMMAND ----------
+
+# Build the best model (balanced training and full test datasets)
+train_pred_b = cvModel_b.transform(train_b)
+test_pred_b = cvModel_b.transform(test)
+
+# Evaluate the model on the balanced training datasets
+pr_train_b = evaluatorPR.evaluate(train_pred_b)
+auc_train_b = evaluatorAUC.evaluate(train_pred_b)
+
+# Evaluate the model on full test datasets
+pr_test_b = evaluatorPR.evaluate(test_pred_b)
+auc_test_b = evaluatorAUC.evaluate(test_pred_b)
+
+# Print out the PR and AUC values
+print("PR train:", pr_train_b)
+print("AUC train:", auc_train_b)
+print("PR test:", pr_test_b)
+print("AUC test:", auc_test_b)
+
+# COMMAND ----------
+
+# Create temporary view for test predictions
+test_pred_b.createOrReplaceTempView("test_pred_b")
+
+# Create test predictions confusion matrix
+test_pred_b_cmdf = spark.sql("select a.label, a.prediction, coalesce(b.count, 0) as count from cmt a left outer join (select label, prediction, count(1) as count from test_pred_b group by label, prediction) b on b.label = a.label and b.prediction = a.prediction order by a.label desc, a.prediction desc")
+
+# View confusion matrix
+display(test_pred_b_cmdf)
+
+# COMMAND ----------
+
+# Convert to pandas
+cm_b_pdf = test_pred_b_cmdf.toPandas()
+
+# Create 1d numpy array of confusion matrix values
+cm_b_1d = cm_b_pdf.iloc[:, 2]
+
+# Create 2d numpy array of confusion matrix vlaues
+cm_b = np.reshape(cm_b_1d, (-1, 2))
+
+# Print out the 2d array
+print(cm_b)
+
+
+# COMMAND ----------
+
+# MAGIC %md ###View the Decision Tree Models
+# MAGIC Visually compare the differences between the unbalanced and balanced decision tree models (basd on the train and train_b datasets respectively).
+
+# COMMAND ----------
+
+# Extract Feature Importance
+#  Attribution: Feature Selection Using Feature Importance Score - Creating a PySpark Estimator
+#               https://www.timlrx.com/2018/06/19/feature-selection-using-feature-importance-score-creating-a-pyspark-estimator/
+import pandas as pd
+
+def ExtractFeatureImp(featureImp, dataset, featuresCol):
+    list_extract = []
+    for i in dataset.schema[featuresCol].metadata["ml_attr"]["attrs"]:
+        list_extract = list_extract + dataset.schema[featuresCol].metadata["ml_attr"]["attrs"][i]
+    varlist = pd.DataFrame(list_extract)
+    varlist['score'] = varlist['idx'].apply(lambda x: featureImp[x])
+    return(varlist.sort_values('score', ascending = False))
+
+# COMMAND ----------
+
+# View the Unbalanced Decision Tree model (prior to CrossValidator)
+dt_model = pipeline.fit(train)
+display(dt_model.stages[-1])
+
+# COMMAND ----------
+
+# Extract Feature Importance for the original unbalanced dt_model
+ExtractFeatureImp(dt_model.stages[-1].featureImportances, train_pred, "features").head(10)
+
+# COMMAND ----------
+
+# View the Balanced Decision Tree model (prior to CrossValidator)
+dt_model_b = pipeline.fit(train_b)
+display(dt_model_b.stages[-1])
+
+# COMMAND ----------
+
+# Extract Feature Importance for the nbalanced dt_model
+ExtractFeatureImp(dt_model_b.stages[-1].featureImportances, train_pred_b, "features").head(10)
+
+# COMMAND ----------
+
+# MAGIC %md ###Comparing Confusion Matrices
+# MAGIC Below we will compare the unbalanced and balanced decision tree ML model confusion matrices.
+
+# COMMAND ----------
+
+# Plot confusion matrix  
+plot_confusion_matrix(cm, "Confusion Matrix (Unbalanced Test)")
+
+# COMMAND ----------
+
+# Plot confusion matrix  
+plot_confusion_matrix(cm_b, "Confusion Matrix (Balanced Test)")
+
+# COMMAND ----------
+
+# Log MLflow
+with mlflow.start_run(experiment_id = mlflow_experiment_id) as run:
+  # Log Parameters and metrics
+  mlflow.log_param("balanced", "yes")
+  mlflow.log_metric("PR train", pr_train_b)
+  mlflow.log_metric("AUC train", auc_train_b)
+  mlflow.log_metric("PR test", pr_test_b)
+  mlflow.log_metric("AUC test", auc_test_b)
+    
+  # Log model
+  mlflow.spark.log_model(dt_model_b, "model")
+  
+  # Log Confusion matrix
+  mlflow.log_artifact("confusion-matrix.png")%md ##Internal use only - File Operations
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+#display(dbutils.fs.ls("dbfs:/FileStore/tables/csv/sao"))
+
+# COMMAND ----------
+
+#dbutils.fs.mkdirs("/FileStore/tables/csv/sao")
+
+# COMMAND ----------
+
+#%fs wget https://www.sao.wa.gov/wp-content/uploads/2018/09/WSAO_LogoLargerIcon-1.png
+#dbutils.fs.cp 
